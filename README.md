ef7eb42873778c2eaef82dc895a5548f184bca34
Hampshire, Jason C <97221507+jasonh4882@users.noreply.github.com>
2022-01-09 02:45:50 -0500

    transfer commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # sciAndEngScienceFairSEFIWebsiteFall2020
+Presentations:
+https://youtu.be/QU950J0SO2c
+https://youtu.be/TOnA4ERuecI
+https://youtu.be/zPURaTctIdk
---
+++ assets/broadcast/index.php
@@ -0,0 +1,349 @@
+<?php
+	include "header.php";
+?>
+
+	<body>
+
+		<!-- Header -->
+			<header id="header">
+				<h1><a href="#">Broadcast <span>by TEMPLATED</span></a></h1>
+				<a href="#menu">Menu</a>
+			</header>
+
+<?php
+	include "menu.php";
+?>
+			<!-- Banner -->
+			<!--
+				To use a video as your background, set data-video to the name of your video without
+				its extension (eg. images/banner). Your video must be available in both .mp4 and .webm
+				formats to work correctly.
+			-->
+				<section id="banner" data-video="images/banner">
+					<div class="inner">
+						<header>
+							<h1>This is Broadcast</h1>
+							<p>Morbi eu purus eget urna interdum dignissim sed consectetur augue<br />
+							vivamus vitae libero in nulla iaculis eleifend non sit amet nulla.</p>
+						</header>
+						<a href="#main" class="button big alt scrolly">Dignissim</a>
+					</div>
+
+				</section>
+
+		<!-- Main -->
+			<div id="main">
+
+			<!-- One -->
+				<section class="wrapper style1">
+					<div class="inner">
+						<header class="align-center">
+							<h2>Nam eu nisi non ante sodale</h2>
+							<p>Cras sagittis turpis sit amet est tempus, sit amet consectetur purus tincidunt.</p>
+						</header>
+						<!-- 2 Column Video Section -->
+							<div class="flex flex-2">
+								<div class="video col">
+									<div class="image fit">
+										<img src="images/pic07.jpg" alt="" />
+										<div class="arrow">
+											<div class="icon fa-play"></div>
+										</div>
+									</div>
+									<p class="caption">
+										Pellentesque at nunc vitae urna suscipit mollis nec in arcu
+									</p>
+									<a href="generic.html" class="link"><span>Click Me</span></a>
+								</div>
+								<div class="video col">
+									<div class="image fit">
+										<img src="images/pic08.jpg" alt="" />
+										<div class="arrow">
+											<div class="icon fa-play"></div>
+										</div>
+									</div>
+									<p class="caption">
+										Morbi mattis ligula ut eros ipsum aliquam iaculis dictum suscipit
+									</p>
+									<a href="generic.html" class="link"><span>Click Me</span></a>
+								</div>
+							</div>
+					</div>
+				</section>
+
+			<!-- Two -->
+				<section class="wrapper style2">
+					<div class="inner">
+						<header>
+							<h2>Etiam veroeros lorem</h2>
+							<p>Fusce fringilla enim sit amet lectus sollicitudin, eu tincidunt odio semper.</p>
+						</header>
+						<!-- Tabbed Video Section -->
+							<div class="flex flex-tabs">
+								<ul class="tab-list">
+									<li><a href="#" data-tab="tab-1" class="active">Duis vestibulum tellus</a></li>
+									<li><a href="#" data-tab="tab-2">Quam tempus sodales ipsum</a></li>
+									<li><a href="#" data-tab="tab-3">Donec faucibus risus cursus</a></li>
+								</ul>
+								<div class="tabs">
+
+									<!-- Tab 1 -->
+										<div class="tab tab-1 flex flex-3 active">
+											<!-- Video Thumbnail -->
+												<div class="video col">
+													<div class="image fit">
+														<img src="images/pic01.jpg" alt="" />
+														<div class="arrow">
+															<div class="icon fa-play"></div>
+														</div>
+													</div>
+													<a href="generic.html" class="link"><span>Click Me</span></a>
+												</div>
+											<!-- Video Thumbnail -->
+												<div class="video col">
+													<div class="image fit">
+														<img src="images/pic02.jpg" alt="" />
+														<div class="arrow">
+															<div class="icon fa-play"></div>
+														</div>
+													</div>
+													<a href="generic.html" class="link"><span>Click Me</span></a>
+												</div>
+											<!-- Video Thumbnail -->
+												<div class="video col">
+													<div class="image fit">
+														<img src="images/pic03.jpg" alt="" />
+														<div class="arrow">
+															<div class="icon fa-play"></div>
+														</div>
+													</div>
+													<a href="generic.html" class="link"><span>Click Me</span></a>
+												</div>
+											<!-- Video Thumbnail -->
+												<div class="video col">
+													<div class="image fit">
+														<img src="images/pic04.jpg" alt="" />
+														<div class="arrow">
+															<div class="icon fa-play"></div>
+														</div>
+													</div>
+													<a href="generic.html" class="link"><span>Click Me</span></a>
+												</div>
+											<!-- Video Thumbnail -->
+												<div class="video col">
+													<div class="image fit">
+														<img src="images/pic05.jpg" alt="" />
+														<div class="arrow">
+															<div class="icon fa-play"></div>
+														</div>
+													</div>
+													<a href="generic.html" class="link"><span>Click Me</span></a>
+												</div>
+											<!-- Video Thumbnail -->
+												<div class="video col">
+													<div class="image fit">
+														<img src="images/pic06.jpg" alt="" />
+														<div class="arrow">
+															<div class="icon fa-play"></div>
+														</div>
+													</div>
+													<a href="generic.html" class="link"><span>Click Me</span></a>
+												</div>
+										</div>
+
+									<!-- Tab 2 -->
+										<div class="tab tab-2 flex flex-3">
+											<!-- Video Thumbnail -->
+												<div class="video col">
+													<div class="image fit">
+														<img src="images/pic06.jpg" alt="" />
+														<div class="arrow">
+															<div class="icon fa-play"></div>
+														</div>
+													</div>
+													<a href="generic.html" class="link"><span>Click Me</span></a>
+												</div>
+											<!-- Video Thumbnail -->
+												<div class="video col">
+													<div class="image fit">
+														<img src="images/pic05.jpg" alt="" />
+														<div class="arrow">
+															<div class="icon fa-play"></div>
+														</div>
+													</div>
+													<a href="generic.html" class="link"><span>Click Me</span></a>
+												</div>
+											<!-- Video Thumbnail -->
+												<div class="video col">
+													<div class="image fit">
+														<img src="images/pic04.jpg" alt="" />
+														<div class="arrow">
+															<div class="icon fa-play"></div>
+														</div>
+													</div>
+													<a href="generic.html" class="link"><span>Click Me</span></a>
+												</div>
+											<!-- Video Thumbnail -->
+												<div class="video col">
+													<div class="image fit">
+														<img src="images/pic03.jpg" alt="" />
+														<div class="arrow">
+															<div class="icon fa-play"></div>
+														</div>
+													</div>
+													<a href="generic.html" class="link"><span>Click Me</span></a>
+												</div>
+											<!-- Video Thumbnail -->
+												<div class="video col">
+													<div class="image fit">
+														<img src="images/pic02.jpg" alt="" />
+														<div class="arrow">
+															<div class="icon fa-play"></div>
+														</div>
+													</div>
+													<a href="generic.html" class="link"><span>Click Me</span></a>
+												</div>
+											<!-- Video Thumbnail -->
+												<div class="video col">
+													<div class="image fit">
+														<img src="images/pic01.jpg" alt="" />
+														<div class="arrow">
+															<div class="icon fa-play"></div>
+														</div>
+													</div>
+													<a href="generic.html" class="link"><span>Click Me</span></a>
+												</div>
+										</div>
+
+									<!-- Tab 3 -->
+										<div class="tab tab-3 flex flex-3">
+											<!-- Video Thumbnail -->
+												<div class="video col">
+													<div class="image fit">
+														<img src="images/pic03.jpg" alt="" />
+														<div class="arrow">
+															<div class="icon fa-play"></div>
+														</div>
+													</div>
+													<a href="generic.html" class="link"><span>Click Me</span></a>
+												</div>
+											<!-- Video Thumbnail -->
+												<div class="video col">
+													<div class="image fit">
+														<img src="images/pic02.jpg" alt="" />
+														<div class="arrow">
+															<div class="icon fa-play"></div>
+														</div>
+													</div>
+													<a href="generic.html" class="link"><span>Click Me</span></a>
+												</div>
+											<!-- Video Thumbnail -->
+												<div class="video col">
+													<div class="image fit">
+														<img src="images/pic01.jpg" alt="" />
+														<div class="arrow">
+															<div class="icon fa-play"></div>
+														</div>
+													</div>
+													<a href="generic.html" class="link"><span>Click Me</span></a>
+												</div>
+											<!-- Video Thumbnail -->
+												<div class="video col">
+													<div class="image fit">
+														<img src="images/pic06.jpg" alt="" />
+														<div class="arrow">
+															<div class="icon fa-play"></div>
+														</div>
+													</div>
+													<a href="generic.html" class="link"><span>Click Me</span></a>
+												</div>
+											<!-- Video Thumbnail -->
+												<div class="video col">
+													<div class="image fit">
+														<img src="images/pic05.jpg" alt="" />
+														<div class="arrow">
+															<div class="icon fa-play"></div>
+														</div>
+													</div>
+													<a href="generic.html" class="link"><span>Click Me</span></a>
+												</div>
+											<!-- Video Thumbnail -->
+												<div class="video col">
+													<div class="image fit">
+														<img src="images/pic04.jpg" alt="" />
+														<div class="arrow">
+															<div class="icon fa-play"></div>
+														</div>
+													</div>
+													<a href="generic.html" class="link"><span>Click Me</span></a>
+												</div>
+										</div>
+
+								</div>
+							</div>
+					</div>
+				</section>
+
+			<!-- Three -->
+				<section class="wrapper ">
+					<div class="inner">
+						<header class="align-center">
+							<h2>Aliquam ipsum purus dolor</h2>
+							<p>Cras sagittis turpis sit amet est tempus, sit amet consectetur purus tincidunt.</p>
+						</header>
+
+						<!-- 3 Column Video Section -->
+							<div class="flex flex-3">
+								<div class="video col">
+									<div class="image fit">
+										<img src="images/pic09.jpg" alt="" />
+										<div class="arrow">
+											<div class="icon fa-play"></div>
+										</div>
+									</div>
+									<p class="caption">
+										Cras eget lacus sed mauris scelerisque tincidunt
+									</p>
+									<a href="generic.html" class="link"><span>Click Me</span></a>
+								</div>
+								<div class="video col">
+									<div class="image fit">
+										<img src="images/pic10.jpg" alt="" />
+										<div class="arrow">
+											<div class="icon fa-play"></div>
+										</div>
+									</div>
+									<p class="caption">
+										Vivamus vulputate lacus non massa auctor lobortis
+									</p>
+									<a href="generic.html" class="link"><span>Click Me</span></a>
+								</div>
+								<div class="video col">
+									<div class="image fit">
+										<img src="images/pic11.jpg" alt="" />
+										<div class="arrow">
+											<div class="icon fa-play"></div>
+										</div>
+									</div>
+									<p class="caption">
+										Nam eu nisi non ante sodales interdum a vitae neque
+									</p>
+									<a href="generic.html" class="link"><span>Click Me</span></a>
+								</div>
+							</div>
+					</div>
+				</section>
+
+			</div>
+<?php
+	include "footer.php";
+?>
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
